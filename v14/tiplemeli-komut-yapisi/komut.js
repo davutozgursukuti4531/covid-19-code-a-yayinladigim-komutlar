2b0ae579bcedbed3da767784c84cd1cb8e8ef7ef
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2022-11-07 14:38:18 +0300

    Create komut.js

---
+++ v14/tiplemeli-komut-yapisi/komut.js
@@ -0,0 +1,14 @@
+//slashlı versiyon:
+/**
+* @type {import("../types/CommandTypes.ts").SlashCommandInterface}
+*/
+module.exports = {
+    komutAyarlari
+}
+//messageli versiyon:
+/**
+* @type {import("../types/CommandTypes.ts").MessageCommandInterface}
+*/
+module.exports = {
+    komutAyarlari
+}
